bec7b2283ada6d7451940d40a5cdb740257b335d
CarlosBotelho42 <carlospiece55@gmail.com>
2021-02-13 16:45:55 -0300

    Testando promeira classe de Dominio(Categories)

--- src/main/java/com/carlosbotelho/curseprojec/resources/CategoryResource.java
+++ src/main/java/com/carlosbotelho/curseprojec/resources/CategoryResource.java
@@ -1,18 +1,30 @@
 package com.carlosbotelho.curseprojec.resources;
 
-import org.springframework.stereotype.Controller;
+import com.carlosbotelho.curseprojec.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping (value = "/categories")
 public class CategoryResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String listar (){
-        return "Rertornooooooooooo";
+    @GetMapping
+    public List<Category> listar (){
+
+        Category cat1 = new Category(1,"Eletronicos");
+        Category cat2 = new Category(2,"Livros");
+
+        List<Category> list = new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
+
     }
 
 }
