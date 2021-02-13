90b79d8caee855238eb1f7d5674ce2bd44bf727c
CarlosBotelho42 <carlospiece55@gmail.com>
2021-02-13 15:35:36 -0300

    Projeto criadogit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit status

---
+++ src/main/java/com/carlosbotelho/curseprojec/CurseprojecApplication.java
@@ -0,0 +1,13 @@
+package com.carlosbotelho.curseprojec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurseprojecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurseprojecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosbotelho/curseprojec/CurseprojecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosbotelho.curseprojec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseprojecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
