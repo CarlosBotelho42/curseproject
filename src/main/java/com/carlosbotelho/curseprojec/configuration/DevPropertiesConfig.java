d33efd32725068296f98bf99b958dcd47aa92f26
CarlosBotelho42 <carlospiece55@gmail.com>
2021-02-22 01:06:32 -0300

    Criado o profile(properties) de desenvolviment

---
+++ src/main/java/com/carlosbotelho/curseprojec/configuration/DevPropertiesConfig.java
@@ -0,0 +1,32 @@
+package com.carlosbotelho.curseprojec.configuration;
+
+import com.carlosbotelho.curseprojec.services.util.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevPropertiesConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if(!"create".equals(strategy)){
+            return false;
+        }
+        dbService.instantiateDatabase();
+        return true;
+
+    }
+
+}
